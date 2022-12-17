c8ac56c4645bd29f53bb230f2aeba8e61fa782bb
Nuwan <nuwan199044@gmail.com>
2022-12-17 09:52:30 +0530

    Hooks - useReduser strict action types

--- src/state/Counter.tsx
+++ src/state/Counter.tsx
@@ -5,10 +5,16 @@ type CounterState = {
     count: number
 }
 
-type CounterAction = {
-    type: string, 
+type UpdateAction = {
+    type: 'increment' | 'decrement' | 'reset', 
     payload: number
 }
+
+type ResetAction = {
+    type: 'reset'
+}
+
+type CounterAction = UpdateAction | ResetAction
 
 const initialState = {
     count : 0
@@ -19,7 +25,9 @@ function reducer(state: CounterState, action: CounterAction){
         case 'increment': 
             return { count: state.count + action.payload }
         case 'decrement': 
-            return { count: state.count - action.payload}
+            return { count: state.count - action.payload }
+        case 'reset':
+            return initialState
         default:
             return state
     }
@@ -32,6 +40,7 @@ export const Counter = () => {
             Count: {state.count}
             <button onClick={() => dispatch({type: 'increment', payload: 10})}>Increment 10</button>
             <button onClick={() => dispatch({type: 'decrement', payload: 10})}>Decrement 10</button>
+            <button onClick={() => dispatch({type: 'reset'})}>Reset</button>
         </>
     )
 }
