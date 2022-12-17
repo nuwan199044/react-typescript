03337fe5ea2b2377ec7c4c06d6ac929c3638f575
Nuwan <nuwan199044@gmail.com>
2022-12-17 09:06:25 +0530

    Hooks - useState

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { Header } from './components/Header';
 import { Button } from './components/Button';
 import { Input } from './components/Input';
 import { Container } from './components/Container';
+import { LoggedIn } from './state/LoggedIn';
+import { User } from './User';
+import { Counter } from './state/Counter';
 
 
 function App() {
@@ -34,7 +37,7 @@ function App() {
 
   return (
     <div className="App">
-      <Greet name='Nuwan' isLoggedIn={true} />
+      {/* <Greet name='Nuwan' isLoggedIn={true} />
       <Person name={person} />
       <Persons names={personList} />
       <Staus status="loading" />
@@ -48,7 +51,8 @@ function App() {
         }} 
       />
       <Input value='' handleChange={(event) => console.log(event)} />
-      <Container styles={{border: '1px solid black', padding: '1rem'}} />
+      <Container styles={{border: '1px solid black', padding: '1rem'}} /> */}
+      <LoggedIn />
     </div>
   );
 }
